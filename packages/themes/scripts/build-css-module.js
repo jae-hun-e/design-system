0a44fc3567b20022587d25c2b88eb7f22f36cd2b
hun2__2 <wognskec@hanyang.ac.kr>
2023-12-24 20:33:02 +0900

    [Feat] typograph 토큰 추가
    1. font-size, font-weight, font-height 속성값 obj로 생성
    2. 위의 값을 엮어서 하나의 토큰으로 만든 obj 생성
    3. 생성한 obj를 기반으로 css를 생성

--- packages/themes/scripts/build-css-module.js
+++ packages/themes/scripts/build-css-module.js
@@ -31,25 +31,65 @@ const generateThemeCssVariables = () => {
   const cssString = [];
 
   Object.entries(theme.vars).forEach(([key, value]) => {
+    const selector = ":root";
     if (key === "colors") {
       Object.entries(value.$static).forEach(([colorKey, colorValue]) => {
         if (colorKey === "light") {
-          const selector = ":root";
           return cssString.push(`${selector} {\n${cssVariables(colorValue)}\n}`);
         }
         if (colorKey === "dark") {
-          const selector = ":root .theme-dark";
-          return cssString.push(`${selector} {\n${cssVariables(colorValue)}\n}`);
+          const selectorDark = selector + " .theme-dark";
+          return cssString.push(`${selectorDark} {\n${cssVariables(colorValue)}\n}`);
         }
+        return;
       });
+      return;
     }
+
+    return cssString.push(`${selector} {\n${cssVariables(value)}\n}`);
   });
   return cssString;
 };
 
-const generateThemeCss = () => {
-  const variables = generateThemeCssVariables();
-  fs.writeFileSync("dist/themes.css", [...variables].join("\n"));
+// 만들 형식 : theme.css
+// :heading-xl {
+// font-size: 3rem;
+// font-weight: 700;
+// line-height: 100%;
+// }
+
+const generateThemeClasses = () => {
+  const cssString = [];
+
+  Object.entries(theme.classes).forEach(([, value]) => {
+    const cssClasses = Object.entries(value)
+      .map(([mainKey, mainValue]) =>
+        Object.entries(mainValue)
+          .map(([subKey, subValue]) => {
+            const className = `.${toCssCasting(mainKey)}-${toCssCasting(subKey)}`;
+
+            const styleProperties = Object.entries(subValue)
+              .map(
+                ([styleKey, styleValue]) => `${toCssCasting(styleKey)}: ${styleValue};`
+              )
+              .join("\n");
+
+            return `${className} {\n${styleProperties}\n}`;
+          })
+          .join("\n")
+      )
+      .join("\n");
+
+    cssString.push(cssClasses);
+  });
+  return cssString;
 };
 
-generateThemeCss();
+const generateThemeCSS = () => {
+  const variables = generateThemeCssVariables();
+
+  const classes = generateThemeClasses();
+  fs.writeFileSync("dist/themes.css", [...variables, ...classes].join("\n"));
+};
+
+generateThemeCSS();
